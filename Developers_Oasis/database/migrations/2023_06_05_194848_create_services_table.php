19eb63807c32b1c52eddc3b355874400a9565d0f
mohmedsabryy <m7mdsabryyy@gmail.com>
2023-06-05 23:01:38 +0300

    add services migrations

---
+++ Developers_Oasis/database/migrations/2023_06_05_194848_create_services_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::disableForeignKeyConstraints();
+
+        Schema::create('services', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('description');
+            $table->bigInteger('price');
+            $table->bigInteger('expert_id');
+            $table->foreign('expert_id')->references('id')->on('experts');
+        });
+
+        Schema::enableForeignKeyConstraints();
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('services');
+    }
+};
