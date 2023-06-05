4a2f26e2126a31bb6cada888d23934fa96bd4c31
unknown <hebaalhwai96@gmail.com>
2023-06-05 22:38:02 +0300

    add database relations

--- Developers_Oasis/app/Models/Post.php
+++ Developers_Oasis/app/Models/Post.php
@@ -4,8 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphMany;
+use Cviebrock\EloquentSluggable\Sluggable;
 
 class Post extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['title','body','user_id','dep_id'];
+
+    function  user(){
+
+        return $this->belongsTo(User::class);
+    }
+    public function comments(): MorphMany
+    {
+        return $this->morphMany(Comment::class, 'commentable');
+    }
+
 }
