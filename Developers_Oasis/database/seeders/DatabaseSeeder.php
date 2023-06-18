b1c1855110867fe14a3ac5609170de9cca5eae05
mohmedsabryy <m7mdsabryyy@gmail.com>
2023-06-18 22:52:13 +0300

    posts logic update

--- Developers_Oasis/database/seeders/DatabaseSeeder.php
+++ Developers_Oasis/database/seeders/DatabaseSeeder.php
@@ -16,8 +16,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-    //    \App\Models\User::factory(20)->create();
-    //     \App\Models\Department::factory(20)->create();
+       // \App\Models\User::factory(20)->create();
+        // \App\Models\Department::factory(20)->create();
         // \App\Models\Expert::factory(10)->create();
         //  \App\Models\Service::factory(10)->create();
         // \App\Models\Contract::factory(20)->create();
