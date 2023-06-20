9705f0f37ee6af9d075d3fa513ef7d66ced5ee6f
Nora Mohammed <NoorMohammed3399@gmail.com>
2023-06-20 14:37:25 +0300

    modify package.json

--- Developers_Oasis/resources/js/app.js
+++ Developers_Oasis/resources/js/app.js
@@ -3,17 +3,6 @@
  * includes Vue and other libraries. It is a great starting point when
  * building robust, powerful web applications using Vue and Laravel.
  */
-import 'vuetify/styles'
-import { createVuetify } from 'vuetify'
-import router from "./routes/index.js";
-
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
-
-const vuetify = createVuetify({
-    components,
-    directives
-})
 
 import './bootstrap';
 import { createApp } from 'vue';
@@ -29,31 +18,6 @@ const app = createApp({});
 import ExampleComponent from './components/ExampleComponent.vue';
 app.component('example-component', ExampleComponent);
 
-import PostComponent from './components/UserComponents/PostComponent.vue';
-app.component('post-component',PostComponent);
-
-import ServicesComponent from "./components/services/ServicesComponent.vue";
-app.component('services-component', ServicesComponent)
-import AddPostComponent from "./components/UserComponents/AddPostComponent.vue";
-app.component('addpost-component',AddPostComponent);
-
-import MenuBarComponent from "./components/MenuBarComponent.vue";
-app.component('menubar-component',MenuBarComponent);
-
-import LoginComponent from "./components/AdminComponents/LoginComponent.vue";
-app.component('login-component',LoginComponent);
-
-import DashBoardComponent from "./components/AdminComponents/DashBoardComponent.vue";
-app.component('dashboard-component',DashBoardComponent);
-
-import AdminHeaderComponent from "./components/AdminComponents/AdminHeaderComponent.vue";
-app.component('adminheader-component',AdminHeaderComponent)
-
-import MainComponent from './components/MainComponent.vue'
-app.component('main-component', MainComponent)
-
-import departmentlist from './components/DepartmentComponents/departmentlist.vue'
-app.component('department-component', departmentlist)
 /**
  * The following block of code may be used to automatically register your
  * Vue components. It will recursively scan this directory for the Vue
@@ -63,8 +27,7 @@ app.component('department-component', departmentlist)
  */
 
 // Object.entries(import.meta.glob('./**/*.vue', { eager: true })).forEach(([path, definition]) => {
-//     app.componeimport { vue } from '@vitejs/plugin-vue';
-// nt(path.split('/').pop().replace(/\.\w+$/, ''), definition.default);
+//     app.component(path.split('/').pop().replace(/\.\w+$/, ''), definition.default);
 // });
 
 /**
@@ -72,5 +35,5 @@ app.component('department-component', departmentlist)
  * an "id" attribute of "app". This element is included with the "auth"
  * scaffolding. Otherwise, you will need to add an element yourself.
  */
-app.use(router);
-app.use(vuetify).mount('#app');
+
+app.mount('#app');
