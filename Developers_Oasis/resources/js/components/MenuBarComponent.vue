dabc2d97d50b0de823e8327bf9f0a371d2877df9
Nora Mohammed <NoorMohammed3399@gmail.com>
2023-06-24 17:10:31 +0300

    update in navbar

--- Developers_Oasis/resources/js/components/MenuBarComponent.vue
+++ Developers_Oasis/resources/js/components/MenuBarComponent.vue
@@ -1,32 +1,36 @@
 <template>
-    <ul class="navbar-nav">
-        <li class="nav-item">
-            <router-link to="/home" class="nav-link">
-                Home
-            </router-link>
-        </li>
-        <li class="nav-item">
-            <router-link to="/services" class="nav-link">
-                Services
-            </router-link>
-        </li>
-        <li class="nav-item">
-            <router-link to="/posts" class="nav-link">
-                Posts
-            </router-link>
-        </li>
-        <li class="nav-item">
-            <router-link to="/experts" class="nav-link">
-                Experts
-            </router-link>
-        </li>
-        <li class="nav-item">
-            <router-link to="/departments" class="nav-link">
-                Departments
-            </router-link>
-        </li>
-
-    </ul>
+    <head>
+        <link rel="stylesheet" href="../../css/app.css">
+    </head>
+    <div class="w-100 d-flex justify-content-center">
+        <ul class="navbar-nav ">
+            <li class="nav-item">
+                <router-link to="/home" class="nav-link">
+                    Home
+                </router-link>
+            </li>
+            <li class="nav-item">
+                <router-link to="/services" class="nav-link">
+                    Services
+                </router-link>
+            </li>
+            <li class="nav-item">
+                <router-link to="/posts" class="nav-link">
+                    Posts
+                </router-link>
+            </li>
+            <li class="nav-item">
+                <router-link to="/experts" class="nav-link">
+                    Experts
+                </router-link>
+            </li>
+            <li class="nav-item">
+                <router-link to="/departments" class="nav-link">
+                    Departments
+                </router-link>
+            </li>
+        </ul>
+    </div>
 </template>
 <script setup>
 </script>
